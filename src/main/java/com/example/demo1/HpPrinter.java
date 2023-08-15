371cf60d51ab4adca53ab08dc8b276675c51de59
JerryLiang317 <a0988768311@gmail.com>
2023-08-16 01:22:20 +0800

    Spring AOP

--- src/main/java/com/example/demo1/HpPrinter.java
+++ src/main/java/com/example/demo1/HpPrinter.java
@@ -1,24 +1,14 @@
 package com.example.demo1;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 
 @Component
 public class HpPrinter implements Printer{
 
-    @Value("${printer.name}")
-    private String name;
-
-    @Value("${printer.count}")
-    private int count;
-
 
     @Override
     public void print(String message) {
-        count--;
-        System.out.println(name + ": " + message);
-        System.out.println("剩餘使用次數:" + count);
+        System.out.println("HP 印表機:" + message);
     }
 }
---
+++ src/main/java/com/example/demo1/MyAspect.java
@@ -0,0 +1,26 @@
+package com.example.demo1;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Aspect
+@Component
+public class MyAspect {
+
+    @Around("execution(* com.example.demo1.HpPrinter.*(..))")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        Date start = new Date();
+
+        Object obj = pjp.proceed();
+
+        Date end = new Date();
+        long time = end.getTime() - start.getTime();
+        System.out.println("總共執行了:" + time + "ms");
+
+        return obj;
+    }
+}
