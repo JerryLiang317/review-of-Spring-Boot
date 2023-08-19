e86d6393c61e7434cc3e1ccca0e042278f8eaed0
JerryLiang317 <a0988768311@gmail.com>
2023-08-19 09:45:19 +0800

    使用 @ControllerAdvice 及 @ExceptionHandler

--- src/main/java/com/example/demo1/MyController.java
+++ src/main/java/com/example/demo1/MyController.java
@@ -1,39 +1,20 @@
 package com.example.demo1;
 
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
     @RequestMapping("/test1")
-    public String test1(@RequestParam(defaultValue = "10") Integer id,
-                        @RequestParam(required = false) String name){
-        System.out.println("id: " + id);
-        System.out.println("name: " + name);
-        return "Hello test1";
+    public String test1(){
+        throw new RuntimeException("test1 error");
     }
 
     @RequestMapping("/test2")
-    public String test2(@RequestBody Student student){
-        System.out.println("Student id: " + student.getId());
-        System.out.println("Student name: " + student.getName());
-        return "Hello test2";
-    }
-
-    @RequestMapping("/test3")
-    public String test3(@RequestHeader String info){
-        System.out.println("header info: " + info);
-        return "Hello test3";
-    }
-
-    @RequestMapping("/test4/{id}/{name}")
-    public String test4(@PathVariable Integer id,
-                        @PathVariable String name){
-        System.out.println("path id: " + id);
-        System.out.println("path name: " + name);
-        return "Hello test4";
+    public String test2(){
+        throw new IllegalArgumentException("test2 error");
     }
 }
