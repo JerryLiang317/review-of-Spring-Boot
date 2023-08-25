be1ebd5b89090dff2729895b1278e19471a3a661
JerryLiang317 <a0988768311@gmail.com>
2023-08-26 01:29:43 +0800

    使用 spring JPA 操作資料庫-CRUD

--- src/main/java/com/example/demo1/StudentController.java
+++ src/main/java/com/example/demo1/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo1;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StudentController {
@@ -17,5 +15,35 @@ public class StudentController {
         studentRepository.save(student);
 
         return "執行資料庫的 Create 操作";
+    }
+
+    @PutMapping("/students/{studentId}")
+    public String update(@PathVariable Integer studentId,
+                         @RequestBody Student student){
+
+        Student s = studentRepository.findById(studentId).orElse(null);
+
+        if(s != null){
+            s.setName(student.getName());
+            studentRepository.save(s);
+            return "執行資料庫的 Update 操作";
+        } else {
+            return "Update 失敗，數據不存在";
+        }
+    }
+
+    @DeleteMapping("/students/{studentId}")
+    public String delete(@PathVariable Integer studentId){
+
+        studentRepository.deleteById(studentId);
+
+        return "執行資料庫的 Delete 操作";
+    }
+
+    @GetMapping("/student/{studentId}")
+    public Student read(@PathVariable Integer studentId){
+
+        Student student = studentRepository.findById(studentId).orElse(null);
+        return student;
     }
 }
