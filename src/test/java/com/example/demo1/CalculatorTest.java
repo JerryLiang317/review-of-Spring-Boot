4a915b98479f40101b22edb04d310c6597e189b2
JerryLiang317 <a0988768311@gmail.com>
2023-08-26 17:56:35 +0800

    JUnit5-@BeforeEach、@AfterEach、@BeforeAll、@AfterAll、@Disabled、@DisplayName 用法

--- src/test/java/com/example/demo1/CalculatorTest.java
+++ src/test/java/com/example/demo1/CalculatorTest.java
@@ -1,11 +1,14 @@
 package com.example.demo1;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
+    @Disabled
     @Test
     public void add(){
         Calculator calculator = new Calculator();
@@ -17,6 +20,7 @@ public class CalculatorTest {
         assertFalse(result<1);
     }
 
+    @DisplayName("測試除法問題")
     @Test
     public void divide(){
         Calculator calculator = new Calculator();
